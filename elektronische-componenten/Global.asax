d51a7cdd85f1a5f8f0152a946729831f063899bb
wimaerts <wim.aerts@gmail.com>
2015-01-21 19:48:46 +0100

    eerste versie

---
+++ elektronische-componenten/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="elektronische_componenten.MvcApplication" Language="C#" %>
---
+++ elektronische-componenten/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(elektronische_componenten.Startup))]
+namespace elektronische_componenten
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
