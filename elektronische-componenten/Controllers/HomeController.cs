147abfebea805e7e48b335f658ac808a96c9c666
wim <wim.aerts@gmail.com>
2015-02-17 15:37:00 +0100

    About pagina verwijderd + Layout bugs opgelost

--- elektronische-componenten/Controllers/HomeController.cs
+++ elektronische-componenten/Controllers/HomeController.cs
@@ -14,17 +14,8 @@ namespace elektronische_componenten.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
